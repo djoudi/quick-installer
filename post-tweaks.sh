2feae4d4a2ec6892c72b55d4e5d41648e92f5aa9
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-06-12 13:39:35 +0300

    Disable Copy for now

--- post-tweaks.sh
+++ post-tweaks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+old_dir=$(pwd)
+
 # Disable the shopping search plugin (for privacy concerns)
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
@@ -14,3 +16,18 @@ make install
 n stable
 cd '..'
 rm -R  './n'
+
+# # Tweak Copy client
+# /opt/copy-client/CopyAgent -installOverlay
+
+# # Fix libdbusmenu issue for CopyAgent
+# # Source: http://www.webupd8.org/2014/06/fix-copycom-indicator-menu-for-ubuntu.html
+# cd /tmp
+# wget https://github.com/hotice/webupd8/raw/master/libdbusmenu-gtk-$(arch).tar.gz
+# tar -xvf libdbusmenu-gtk*
+# sudo cp /tmp/libdbusmenu-gtk*/* /opt/copy-client/
+
+# # Restart nautilus
+# nautilus -q
+
+cd "$pwd"
